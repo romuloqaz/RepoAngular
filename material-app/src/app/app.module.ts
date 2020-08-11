abf4203289ef4d1bd563d9618d5cbeec396adc28
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-11 11:58:09 -0300

    string interpolation

--- material-app/src/app/app.module.ts
+++ material-app/src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringInterpolationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatCardModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
