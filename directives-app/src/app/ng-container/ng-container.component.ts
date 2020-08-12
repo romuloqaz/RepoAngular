d4d48d26d467cec74f719d2716b33ef3659eba97
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-12 10:23:41 -0300

    ng-container

--- directives-app/src/app/ng-container/ng-container.component.ts
+++ directives-app/src/app/ng-container/ng-container.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgContainerComponent implements OnInit {
 
+  users = [
+    {login: "bob", role: "admin", lastlogin: new Date ('2/1/2018')},
+    {login: "lia", role: "user", lastlogin: new Date ('4/30/2018')},
+    {login: "john", role: "admin", lastlogin: new Date ('4/23/2018')},
+    {login: "robin", role: "user", lastlogin: new Date ('2/30/2018')},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
