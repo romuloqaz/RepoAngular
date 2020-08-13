5edc2758fe687aba16e38da96220f39335cdc8d1
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-13 10:29:56 -0300

    ViewChild

--- communication-app/src/app/parent-child/parent-child.component.ts
+++ communication-app/src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  // @ViewChild("stopwatch2")pegar componentes dentro da template e injetar a instancia dentro da classe
+  @ViewChild(TimerComponent)
+  private mytimer: TimerComponent;
+
+  @ViewChild("myP")
+  private myp: ElementRef
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  start(){
+    this.mytimer.start();
+  }
+
+  stop(){
+    this.mytimer.stop();
+  }
+
+  clear(){
+    this.mytimer.clear();
+  }
+
+  ngAfterViewInit(){
+    //inicializado após todos os componentes serem renderizados
+    console.log(this.myp);
   }
 
 }
