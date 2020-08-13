9f1bd4fc861a506e9789c81f85370925ae174927
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-13 12:28:37 -0300

    Interceptando mudanças no input

--- communication-app/src/app/app.module.ts
+++ communication-app/src/app/app.module.ts
@@ -26,6 +26,8 @@ import { ParentChildComponent } from './parent-child/parent-child.component';
 import { TimerComponent } from './parent-child/timer/timer.component';
 import { OnChangesComponent } from './on-changes/on-changes.component';
 import { NameChangesComponent } from './on-changes/name-changes/name-changes.component';
+import { InterceptingComponent } from './intercepting/intercepting.component';
+import { NameComponent } from './intercepting/name/name.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +41,9 @@ import { NameChangesComponent } from './on-changes/name-changes/name-changes.com
     ParentChildComponent,
     TimerComponent,
     OnChangesComponent,
-    NameChangesComponent
+    NameChangesComponent,
+    InterceptingComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
