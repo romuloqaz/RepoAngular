0a0db4148f45d31748df5ea4c59dc39ec6600cb6
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-16 10:20:39 -0300

    first observable

---
+++ observables/src/app/app.module.ts
@@ -0,0 +1,97 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BasicComponent } from './basic/basic.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatTreeModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatTableModule,
+    MatStepperModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatRippleModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatBadgeModule,
+    BrowserModule,
+    MatBottomSheetModule,
+    MatButtonToggleModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    FlexLayoutModule,
+    MatChipsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ observables/src/app/basic/basic.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+
+@Component({
+  selector: 'app-basic',
+  templateUrl: './basic.component.html',
+  styleUrls: ['./basic.component.css']
+})
+export class BasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const myFirstObservable = new Observable(
+      (observer: Observer<number> )=> {
+        observer.next(1);
+        observer.next(2);
+        observer.next(3);
+        observer.next(4);
+        observer.next(5);
+        observer.error("error");
+        observer.complete();
+      }
+    );
+    myFirstObservable.subscribe((n:number)=>
+      console.log(n),
+      (error) => console.error(error),
+      () => console.log('completed'))
+  }
+
+}
