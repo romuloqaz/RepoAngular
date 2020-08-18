ae6b7003cdca730cce0951ea60b85205ba92e66b
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-18 16:00:55 -0300

    debounce time click e search

--- rxjs/src/app/operators/operators.component.ts
+++ rxjs/src/app/operators/operators.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { from, fromEvent, interval, Observable, Subscription } from 'rxjs';
-import { map, delay, filter, tap, take, first, last } from 'rxjs/operators';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { from, fromEvent, interval, Observable, Subscription, Subject, timer } from 'rxjs';
+import { map, delay, filter, tap, take, first, last, debounceTime, takeWhile, takeUntil } from 'rxjs/operators';
+import { MatRipple } from '@angular/material/core';
 
 @Component({
   selector: 'app-operators',
@@ -9,80 +10,119 @@ import { map, delay, filter, tap, take, first, last } from 'rxjs/operators';
 })
 export class OperatorsComponent implements OnInit {
 
+  @ViewChild(MatRipple) ripple: MatRipple;
+  searchInput: string = '';
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
-  mapClick(){
+  mapClick() {
     from([1,2,3,4,5,6,7])
     .pipe(
-      map(i =>i*2),
-      map(i => i*10),
-      delay(1000)
+      map(i=>i*2),
+      map(i=>i*10),
+      delay(2000)
     )
-    .subscribe(i => console.log(i));
+    .subscribe(i=>console.log(i));
 
     fromEvent(document, 'click')
       .pipe(
         map((e: MouseEvent) => ({x: e.screenX, y: e.screenY}))
       )
-      .subscribe((pos) => console.log(pos)); 
+      .subscribe((pos) => console.log(pos));
   }
 
-  filterClick(){
+  filterClick() {
     from([1,2,3,4,5,6,7])
     .pipe(
-      filter(i => i%2==1)
+      filter(i=>i%2==1)
     )
-    .subscribe(i => console.log(i));
+    .subscribe(i=>console.log(i));
+    
     interval(1000)
-    .pipe(
-      filter(i => i%2==0),
-      map(i => "value: " +i),
-      delay(1000))
-    .subscribe(i=> console.log(i))
+      .pipe(
+        filter(i => i%2==0),
+        map(i=>"Value: " + i),
+        delay(1000))
+      .subscribe(i=>console.log(i));
   }
 
-  tapClick(){
+  tapClick() {
     interval(1000)
-    .pipe(
-      tap(i => console.log('')),
-      tap(i => console.warn('Before filtering: ', i)),
-      filter(i => i%2==0),
-      tap(i => console.warn('after filtering: ', i)),
-      map(i => "value: " +i),
-      tap(i => console.warn('after map: ', i)),
-      delay(1000))
-    .subscribe(i=> console.log(i))
+      .pipe(
+        tap(i => console.log('')),
+        tap(i => console.warn('Before filtering: ', i)),
+        filter(i => i%2==0),
+        tap(i => console.warn('After filtering: ', i)),
+        map(i=>"Value: " + i),
+        tap(i => console.warn('After map: ', i)),
+        delay(1000))
+      .subscribe(i=>console.log(i));    
   }
 
-  takeClick(){
+  takeClick() {
     const observable = new Observable((observer) => {
       let i;
-      for(i=0; i<20; i++)
-        setTimeout(() => observer.next(Math.floor(Math.random()*100)), i*100);
-        setTimeout(()=> observer.complete(), i*100);
+      for(i=0;i<20;i++)
+        setTimeout(()=>observer.next(Math.floor(Math.random()*100)), i*100);
+      //setTimeout(()=>observer.complete(), i*100);
     });
 
     const s: Subscription = observable
       .pipe(
-        tap(i=> console.log(i)),
-        //take(10) // ja faz o onsubscrbe o take
-        //first() //pega o primeiro e faz o subscribe
-        //last() // gera todos os eventos e fas o subscribe apos o ultimo
+        tap(i=>console.log(i)),
+        //take(10)
+        first()
+        //last()
       )
-      .subscribe(v=> console.log('output: ', v),
-      (error)=> console.log(error),
-      () => console.log('Complete!')
+      .subscribe(
+        v=>console.log('Output: ',v),
+        (error) => console.error(error),
+        () => console.log('Compĺete!')
       );
 
-    const interv = setInterval(()=> {
-      console.log('Cheking...');
-      if(s.closed){
-        console.warn('Subscription CLOSEd');
+    const interv = setInterval(()=>{
+      console.log('Checking...');
+      if(s.closed) {
+        console.warn('Subscription CLOSED!');
         clearInterval(interv);
       }
-    }, 200)
+
+    },200)
   }
+
+  launchRipple() {
+    const rippleRef = this.ripple.launch({
+      persistent: true, centered: true });
+    rippleRef.fadeOut();
+  }
+
+  debounceTimeClick() {
+    fromEvent(document, 'click')
+    .pipe(
+      tap((e)=> console.log('Click')),
+      debounceTime(1000)
+    )
+    .subscribe(
+      (e: MouseEvent) => {
+        console.log("Click with debounceTime: ", e);
+        this.launchRipple();
+      })
+  }
+
+  searchEntry$: Subject<string> =  new Subject<string>();
+  searchBy_UsingDebounce(event) {
+    this.searchEntry$.next(this.searchInput);
+  }
+
+  debounceTimeSearch() {
+    this.searchEntry$
+      .pipe(debounceTime(500))
+      .subscribe((s)=> console.log(s))
+  }
+
+
+
 }
