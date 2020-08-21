fd549a5cded82ce8475d7e85ae2cadd6a450e4b2
romuloqaz <romulo.pereiracz@hotmail.com>
2020-08-21 13:03:14 -0300

    tratamento de erros: online e offline

--- http-client/client/app-client/src/app/app.component.ts
+++ http-client/client/app-client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ProductsService } from './products.service';
 import { Observable } from 'rxjs';
 import { Product } from './Product.model';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,11 @@ import { Product } from './Product.model';
 export class AppComponent {
 
   simpleReqProductsObs$: Observable<Product[]>;
+  productsErrorHandling: Product[]
 
   constructor (
-    private productService: ProductsService){}
+    private productService: ProductsService,
+    private snackBar: MatSnackBar){}
     
     ngOnInit(){
       
@@ -21,5 +24,28 @@ export class AppComponent {
 
     getSimpleHttpRequest(){
       this.simpleReqProductsObs$ = this.productService.getProducts();
+    }
+
+    getProductsWithErrorHandling() {
+      this.productService.getProductsError()
+        .subscribe(
+          (prods) => { this.productsErrorHandling = prods; },
+          (err) => {
+            console.log(err);
+            console.log("Message: " + err.error.msg);
+            console.log("Status code: " + err.status);
+            let config = new MatSnackBarConfig();
+            config.duration = 2000;
+            config.panelClass = ['snack_error'];
+  
+            if (err.status == 0)
+              this.snackBar.open('Could not connect to the server', '', config);
+            else
+              this.snackBar.open(err.error.msg, '', config);
+          }
+        )
+    }
+    getProductsWithErrorHandlingOK(){
+
     }
 }
