9d11866477317dd68782034fa8f8c41174287f89
Romulo Pereira <romulo.pereiracz@hotmail.com>
2020-08-23 13:22:36 -0300

    validação do formulario template driven

--- template-driven/src/app/app.module.ts
+++ template-driven/src/app/app.module.ts
@@ -46,12 +46,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { FormNativeValidationComponent } from './form-native-validation/form-native-validation.component';
+import { FormValidationComponent } from './form-validation/form-validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenFormComponent,
-    FormNativeValidationComponent
+    FormNativeValidationComponent,
+    FormValidationComponent
   ],
   imports: [
     BrowserModule,
