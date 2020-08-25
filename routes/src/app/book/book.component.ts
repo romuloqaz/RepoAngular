6a51df991dbd38775c31010742bfef5267a03062
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-25 11:11:34 -0300

    criando lista de livros

--- routes/src/app/book/book.component.ts
+++ routes/src/app/book/book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book.service'
+import { Observable } from 'rxjs';
+import { Book } from '../models/book';
 
 @Component({
   selector: 'app-book',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  book$: Observable<Book[]>
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+    this.book$ = this.bookService.books$;
   }
 
 }
