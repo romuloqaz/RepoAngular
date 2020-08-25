d1f05ac260f16110e90132cd1e34e9a2cf973e90
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-25 11:32:15 -0300

    obtendo parametro nas rotas

--- routes/src/app/dvd/dvd-detail/dvd-detail.component.ts
+++ routes/src/app/dvd/dvd-detail/dvd-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-dvd-detail',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DvdDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    console.log("index: ", this.route.snapshot.paramMap.get('index')); //parametro fixo
+
+    this.route.paramMap
+      .subscribe((params: ParamMap) => console.log("index", params.get('index'))) // parametro avisado quando mudanças
+
   }
 
 }
