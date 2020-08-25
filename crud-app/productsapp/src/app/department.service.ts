0c3188dfaf834f7a7bfd5a48d3599c8f6af0b81d
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-24 21:41:59 -0300

    adicionando department com subject para atualizar no momento da adição

--- crud-app/productsapp/src/app/department.service.ts
+++ crud-app/productsapp/src/app/department.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Department } from './department';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  readonly url = 'http://localhost:3000/departments'
+  readonly url = 'http://localhost:3000/departments';
+
+  private departmentsSubject$: BehaviorSubject<Department[]> = new BehaviorSubject<Department[]>(null);
+  private loaded: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   get(): Observable<Department[]>{
-    return this.http.get<Department[]>(this.url);
+    if(!this.loaded){
+      this.http.get<Department[]>(this.url)
+        .pipe(tap((deps)=> console.log(deps)))
+        .subscribe(this.departmentsSubject$)
+        this.loaded = true;
+    }
+    return this.departmentsSubject$.asObservable();
   }
 
   add(d: Department): Observable<Department>{
     return this.http.post<Department>(this.url, d)
+      .pipe(
+        tap((dep: Department) => this.departmentsSubject$.getValue().push(dep))
+      )
   }
 }
