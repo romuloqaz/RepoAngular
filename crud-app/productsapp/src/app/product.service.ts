44f342a30cebf4e0a139329dc120edf3ac33ed06
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-24 23:25:49 -0300

    service de produto

--- crud-app/productsapp/src/app/product.service.ts
+++ crud-app/productsapp/src/app/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Product } from './product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  readonly url = 'http://localhost:300/products';
+  private productsSubject$: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>(null);
+  private loaded: boolean = false;
+
+  constructor(private http: HttpClient) { }
+
+  get(): Observable<Product[]>{
+    return this.productsSubject$.asObservable();
+  }
 }
