adf42841f9ff2392ac7a72d38d3057696d672640
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-26 18:19:07 -0300

    configurando ngrx

---
+++ rgrx-basic/src/app/store/person.actions.ts
@@ -0,0 +1,6 @@
+export enum PersonActionTypes {
+    PERSON_ALL = '[PERSON_ALL] Get all people',
+    PERSON_NEW = '[PERSON_NEW] Add new person',
+    PERSON_UPDATE = '[PERSON_UPDATE] Update a people',
+    PERSON_DELETE = '[PERSON_DELTE] Delete a person',
+}
