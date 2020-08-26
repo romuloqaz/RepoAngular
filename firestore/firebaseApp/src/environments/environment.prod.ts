1491ecdea804c98ec3442066333ff9dc1823cbf3
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-26 16:37:00 -0300

    projeto para produção

--- firestore/firebaseApp/src/environments/environment.prod.ts
+++ firestore/firebaseApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC8x37vmzLU8VaOf-FLZY1-Xgjxmz0-18g",
+    authDomain: "products-prod-4c06d.firebaseapp.com",
+    databaseURL: "https://products-prod-4c06d.firebaseio.com",
+    projectId: "products-prod-4c06d",
+    storageBucket: "products-prod-4c06d.appspot.com",
+    messagingSenderId: "708808791675",
+    appId: "1:708808791675:web:58fcf034219a7267c134de",
+    measurementId: "G-8S9HY8JQBX"
+  }
 };
