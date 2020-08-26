8cac1c1fc044657603f1f8adc4aa3ecbdb6d3756
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-26 18:43:18 -0300

    actions

--- rgrx-basic/src/app/store/person.actions.ts
+++ rgrx-basic/src/app/store/person.actions.ts
@@ -1,6 +1,31 @@
+import { Action } from '@ngrx/store';
+import { Person } from '../person';
+import { type } from 'os';
+
 export enum PersonActionTypes {
     PERSON_ALL = '[PERSON_ALL] Get all people',
     PERSON_NEW = '[PERSON_NEW] Add new person',
     PERSON_UPDATE = '[PERSON_UPDATE] Update a people',
     PERSON_DELETE = '[PERSON_DELTE] Delete a person',
 }
+
+export class PersonAll implements Action{
+    readonly type = PersonActionTypes.PERSON_ALL;
+}
+
+export class PersonNew implements Action{
+    readonly type = PersonActionTypes.PERSON_NEW;
+    constructor(public payload: {person: Person}){}
+}
+
+export class PersonUpdate implements Action{
+    readonly type = PersonActionTypes.PERSON_UPDATE;
+    constructor(public payload: {person: Person}){}
+}
+
+export class PersonDelete implements Action{
+    readonly type = PersonActionTypes.PERSON_DELETE;
+    constructor(public payload: {id: string}){}
+}
+
+export type PersonActions = PersonAll | PersonNew | PersonUpdate | PersonDelete;
