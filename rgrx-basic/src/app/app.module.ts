b297cdc180b50cead4bf70fda3d89e2b84deb3bd
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-26 17:52:45 -0300

    adicionando recursos

--- rgrx-basic/src/app/app.module.ts
+++ rgrx-basic/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
---
+++ rgrx-basic/src/app/person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+    name: string;
+    age: number;
+    address: string;
+    city: string;
+    country: string;
+    _id ?: string;
+    
+}
