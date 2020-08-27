dde089b3ddd73550f6e7fb502440547e30c7d3f1
Romulo Pereira <romulo.pereiracz@gmail.com>
2020-08-27 09:09:49 -0300

    config firebase

---
+++ fireauth/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDqW8UU0p5eM4yj-4Yr33FJ_-aOVFWSHn4",
+    authDomain: "angular-fireauth-13726.firebaseapp.com",
+    databaseURL: "https://angular-fireauth-13726.firebaseio.com",
+    projectId: "angular-fireauth-13726",
+    storageBucket: "angular-fireauth-13726.appspot.com",
+    messagingSenderId: "324678859717",
+    appId: "1:324678859717:web:16000946ad1f072b5d29ac",
+    measurementId: "G-YV2G2GBK0L"
+  }
+};
---
+++ fireauth/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDqW8UU0p5eM4yj-4Yr33FJ_-aOVFWSHn4",
+    authDomain: "angular-fireauth-13726.firebaseapp.com",
+    databaseURL: "https://angular-fireauth-13726.firebaseio.com",
+    projectId: "angular-fireauth-13726",
+    storageBucket: "angular-fireauth-13726.appspot.com",
+    messagingSenderId: "324678859717",
+    appId: "1:324678859717:web:16000946ad1f072b5d29ac",
+    measurementId: "G-YV2G2GBK0L"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
